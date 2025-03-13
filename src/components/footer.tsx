da4c01b2b78d7479bc6a59b7aa4aea349c4c9c65
cynthiaanareta <cynthia@arteegroup.id>
2025-03-13 16:44:24 +0700

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,19 +3,21 @@ import Link from "next/link"
 export default function Footer() {
   return (
     <footer className="bg-red-800 text-white py-6">
-      <div className="container mx-auto flex justify-center space-x-8 px-4">
-        <Link href="/about" className="hover:underline">
-          About FS Koi
-        </Link>
-        <Link href="/license" className="hover:underline">
-          License/Certificate
-        </Link>
-        <Link href="/terms" className="hover:underline">
-          Terms of Use Agreement
-        </Link>
-        <Link href="/privacy" className="hover:underline">
-          Privacy policy
-        </Link>
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap justify-center gap-4 sm:gap-6 md:gap-8">
+          <Link href="/about" className="hover:underline text-sm sm:text-base whitespace-nowrap py-1">
+            About FS Koi
+          </Link>
+          <Link href="/license" className="hover:underline text-sm sm:text-base whitespace-nowrap py-1">
+            License/Certificate
+          </Link>
+          <Link href="/terms" className="hover:underline text-sm sm:text-base whitespace-nowrap py-1">
+            Terms of Use Agreement
+          </Link>
+          <Link href="/privacy" className="hover:underline text-sm sm:text-base whitespace-nowrap py-1">
+            Privacy policy
+          </Link>
+        </div>
       </div>
     </footer>
   )
